5ac43135ffa8e81176f8cb5e860e7ca52c90acaa
BlueishSapphire <blueishsapphire0@gmail.com>
2023-11-16 17:15:29 -0500

    misc changes

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ import fs from 'node:fs';
 import https from 'node:https';
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+dotenv.config();
 
 
+process.env.NODE_ENV ??= "production";
 console.log(`running in ${process.env.NODE_ENV} environment`);
-
 const isProduction = process.env.NODE_ENV == "production";
 
 
-const port = 3000;
+const port = 443;
 
+console.log(`getting ssl credentials from ${process.env.SSL_CERT} and ${process.env.SSL_KEY}`);
 const sslOptions = isProduction ? {
 	cert: fs.readFileSync(process.env.SSL_CERT),
 	key: fs.readFileSync(process.env.SSL_KEY),
@@ -26,9 +27,8 @@ app.get("/", (req, res) => {
 });
 
 const server = https.createServer(app, sslOptions).listen(port, () => {
-	console.log(`Listening on port ${port}`);
+	console.log(`listening on port ${port}`);
 });
-
 
 process.on("beforeExit", () => {
 	server.close();
