260816d57814a0a10af8ecadaa639f5307ee1472
BlueishSapphire <blueishsapphire0@gmail.com>
2023-11-28 20:30:48 -0500

    no longer keep track of environment

--- index.js
+++ index.js
@@ -2,28 +2,21 @@ import fs from 'node:fs';
 import https from 'node:https';
 import path from 'node:path'
 import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
-
-
-process.env.NODE_ENV ??= "production";
-console.log(`running in ${process.env.NODE_ENV} environment`);
-const isProduction = process.env.NODE_ENV == "production";
 
 
 const port = 443;
 
 const SSL_DIR = "/etc/ssl";
 console.log(`getting ssl credentials from ${SSL_DIR}`);
-const sslOptions = isProduction ? {
+const sslOptions = {
 	cert: fs.readFileSync(path.join(SSL_DIR, "certs/soaper.dev.pem")),
 	key: fs.readFileSync(path.join(SSL_DIR, "private/soaper.dev.pem")),
-} : {};
+};
 
 const app = express();
 
+
 app.get("/", (req, res) => {
-	console.log("new request");
 	res.writeHead(200);
 	res.end("coming soon!\n");
 });
